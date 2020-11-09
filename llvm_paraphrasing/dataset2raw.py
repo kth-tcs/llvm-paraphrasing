137ff9c7b1e1c077315af56c084db2b8fdd65fe1
Orestis Floros <forestis@kth.se>
2020-11-09 22:46:33 +0100

    dataset2raw: Add SentencePiece

--- llvm_paraphrasing/dataset2raw.py
+++ llvm_paraphrasing/dataset2raw.py
@@ -1,13 +1,52 @@
+import argparse
+import atexit
+
 from .utils import read_dataset
 
 
-def main():
+def entry_point():
+    import sys
+
+    main(parse_args(sys.argv[1:]))
+
+
+def main(args):
     d = read_dataset()
+    atexit.register(d.close)
+
+    sp = None
+    if args.sentencepiece:
+        import sentencepiece as spm
+
+        sp = spm.SentencePieceProcessor("m.model")
 
     for filecontent in d.values():
         for funcontent in filecontent:
-            tokens, _ = funcontent
-            if tokens:
-                print(" ".join(tokens))
+            out = " ".join(funcontent[0])
+            if not out:
+                continue
+            if args.sentencepiece:
+                out = " ".join(sp.encode(out, out_type=str))
+            print(out)
 
-    d.close()
+
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+    g = parser.add_mutually_exclusive_group()
+    g.add_argument(
+        "--raw", action="store_true", default=True, help="Sentences as raw output"
+    )
+    # XXX: do it with spm_encode
+    g.add_argument(
+        "--sentencepiece",
+        action="store_true",
+        help="Process sentences with SentencePiece",
+    )
+    # parser.add_argument(
+    #     "--split",
+    #     action="store_true",
+    #     help="Store results in separate files according to optimization level",
+    # )
+
+    args = parser.parse_args()
+    return args
